c952e9933b8a478f9f6254ec2da73efb5d5cc3da
Cabpacob <Cabpacob@github.com>
2022-02-07 01:25:36 +0300

    Added documentation

--- ast_walker/executor.py
+++ ast_walker/executor.py
@@ -3,8 +3,16 @@ import subprocess
 
 
 class FunctionExecutor:
+    '''
+        Executes the function passed to it with the specified parameters
+    '''
     @staticmethod
     def execute_function(name, input_stream, *args):
+        '''
+            name: str - function name
+            input_stream: BytesIO
+            *args - arguments defining the behavior of the function
+        '''
         function = FunctionHolder.get_function(name)
 
         if function is None:
--- ast_walker/holder.py
+++ ast_walker/holder.py
@@ -1,8 +1,14 @@
 class FunctionHolder:
+    '''
+        a class that stores functions that can be called from the CLI
+    '''
     __functions = dict()
 
     @classmethod
     def shell_function(cls, name): #decorator
+        '''
+            a decorator that allows you to turn a custom function into a function for the CLI
+        '''
         def wrapper(function):
             if name in cls.__functions:
                 raise KeyError(f'function {name} already exists')
@@ -13,6 +19,10 @@ class FunctionHolder:
 
     @classmethod
     def get_function(cls, name):
+        '''
+            name:str - function name
+            returns a function or None if no function with the same name has been added
+        '''
         if name not in cls.__functions:
             return None
 
@@ -20,4 +30,7 @@ class FunctionHolder:
 
     @classmethod
     def functions_count(cls):
+        '''
+            returns the number of added functions
+        '''
         return len(cls.__functions)
