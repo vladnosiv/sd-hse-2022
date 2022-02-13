5006b4eb2b3bbc1701ac123b439ea6c093420c8d
Cabpacob <Cabpacob@github.com>
2022-02-14 01:47:28 +0300

    Fixed stderr and BytesIO in executor

--- ast_walker/executor.py
+++ ast_walker/executor.py
@@ -1,4 +1,5 @@
 from ast_walker.holder import FunctionHolder
+from io import BytesIO
 import subprocess
 
 
@@ -16,7 +17,7 @@ class FunctionExecutor:
         function = FunctionHolder.get_function(name)
 
         if function is None:
-            result = subprocess.run([name, input_stream.read()], stdout=subprocess.PIPE)
-            return result.returncode, result.stdout, result.stderr
+            result = subprocess.run([name, input_stream.read()], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            return result.returncode, BytesIO(result.stdout), BytesIO(result.stderr)
         else:
             return function(input_stream, *args)
