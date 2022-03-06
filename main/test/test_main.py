9afbe2ec0723b2c3cfc7439a73a2a8d15fe3ea1e
FiFlyC <combolev@gmail.com>
2022-03-06 16:31:40 +0300

    More tests

--- main/test/test_main.py
+++ main/test/test_main.py
@@ -25,35 +25,45 @@ def main_test(command, expected_out=None):
     sys.stdin = sys.__stdin__
 
 
-def test_echo_noargs_empty_output():
+def test_commands():
     main_test('echo', '> \n> ')
-
-
-def test_echo_five_args_correct_output():
-    main_test('echo 1 2 3 4 5', '> 1 2 3 4 5\n> ')
-
-
-def test_wrong_grammar_error_output():
-    main_test('| kek |', '> Pipe\'s left command can\'t be empty\n> ')
-
-
-def test_simple():
-    main_test('echo "$"', '> Variable name can\'t start with quotes\n> ')
     main_test('echo 123', '> 123\n> ')
+    main_test('echo 1 2 3 4 5', '> 1 2 3 4 5\n> ')
+    main_test('echo 1    2    3', '> 1 2 3\n> ')
+    main_test('echo echo echo echo', '> echo echo echo\n> ')
+    main_test('wc', '> \t0\t0\t0\t\n> ')
+    main_test('wc', '> \t0\t0\t0\t\n> ')
+
+
+def test_quotes():
+    main_test('"e"c\'h\'o 123', '> 123\n> ')
     main_test('echo "12"3', '> 123\n> ')
     main_test('echo \'$x\'', '> $x\n> ')
     main_test('echo \'$env\'var', '> $envvar\n> ')
     main_test('echo \'$env\'v\'a\'r', '> $envvar\n> ')
-    main_test('echo \'$e\'n\'v\'v\'a\'r', '> $envvar\n> ')
-    main_test('echo 1 2 3', '> 1 2 3\n> ')
+    main_test('echo \'$e\'n"v"v\'a\'r', '> $envvar\n> ')
     main_test('\'echo\' 1 2 3', '> 1 2 3\n> ')
-    main_test('\'e\'c"ho" 1 2 3', '> 1 2 3\n> ')
-    main_test('echo 1    2    3', '> 1 2 3\n> ')
-    main_test('wc', '> \t0\t0\t0\t\n> ')
-    main_test('wc', '> \t0\t0\t0\t\n> ')
+    main_test('\'ec\'"ho" 1 2 3', '> 1 2 3\n> ')
+
+    main_test('x=5\nbash -c "echo $x"', '> > 5\n> ')
+    main_test('x=5\nbash -c "echo $x"123', '> > 5123\n> ')
+    main_test('x=5\nbash -c "echo $x"1\'2\'3', '> > 5123\n> ')
+    main_test('x=5\nbash -c "echo $x"1"2"3', '> > 5123\n> ')
+    main_test('x=5\nbash -c \'echo $x\'', '> > 5\n> ')
+    main_test('x=5\nbash -c \"echo \'$x\'\"', '> > 5\n> ')
+    main_test('x=5\nbash -c \"echo \'"$x"\'\"', '> > 5\n> ')
+    
+    main_test('cat ./"  file"', '> file ./  file does not found\n> ')
+
+
+def test_variables_name():
+    main_test('echo "$"', '> Variable name can\'t start with quotes\n> ')
+    main_test('echo $1', '> Variable name can\'t start with digit\n> ')
+    main_test('echo $#', '> Variable name can\'t start with \'#\'\n> ')
 
 
 def test_pipes():
+    main_test('| kek |', '> Pipe\'s left command can\'t be empty\n> ')
     main_test('echo "123" | echo', '> \n> ')
     main_test('/usr/bin/echo "123" | echo', '> \n> ')
     main_test('printf "123" | echo', '> \n> ')
@@ -71,30 +81,30 @@ def test_no_crashes():
     main_test('echo echo echo echo')
     main_test('cat "README"\'.md\'')
     main_test('"e"c\'h\'o 123')
-    main_test('cat ../utils/"meow file"')
     main_test('bash -c \'echo $x\'')
 
 
-def test_check_values():
-    main_test('echo echo echo echo', '> echo echo echo\n> ')
-    main_test('"e"c\'h\'o 123', '> 123\n> ')
-
-
-def test_env():
-    main_test('x=5\nbash -c "echo $x"', '> > 5\n> ')
-    main_test('x=5\nbash -c "echo $x"123', '> > 5123\n> ')
-    main_test('x=5\nbash -c "echo $x"1\'2\'3', '> > 5123\n> ')
-    main_test('x=5\nbash -c "echo $x"1"2"3', '> > 5123\n> ')
-    main_test('x=5\nbash -c \'echo $x\'', '> > 5\n> ')
-    main_test('x=5\nbash -c \"echo \'$x\'\"', '> > 5\n> ')
-    main_test('x=5\nbash -c \"echo \'"$x"\'\"', '> > 5\n> ')
+def test_env_substitution():
     main_test('x=ec\ny=ho\nbash -c \"$x$y \'"$x"\'\"', '> > > ec\n> ')
     main_test('x=ec\ny=ho\n$x$y 123', '> > > 123\n> ')
     main_test('x=ch\ne$x"o" 123', '> > 123\n> ')
     main_test('x=228\ny=$x\nx=322\necho $x$y', '> > > > 322228\n> ')
     main_test('x=echo\ny=" 1   2    3"\n$x$y', '> > > 1 2 3\n> ')
     main_test('x=ec\ny="ho 1   2    3"\n$x$y', '> > > 1 2 3\n> ')
+
+
+def test_assignment():
+    main_test('x =4', '> [Errno 2] No such file or directory: \'x\'\n> ')
+    main_test('x= 4', '> [Errno 2] No such file or directory: \'x=\'\n> ')
+    main_test('=4', '> [Errno 2] No such file or directory: \'=4\'\n> ')
+    main_test('x=', '> [Errno 2] No such file or directory: \'x=\'\n> ')
+    main_test('1=2', '> [Errno 2] No such file or directory: \'1=2\'\n> ')
+    main_test('"abc"=2', '> [Errno 2] No such file or directory: \'abc=2\'\n> ')
+
+    main_test('      x=2\necho $x', '> > 2\n> ')
+    main_test('echo x=2', '> x=2\n> ')
     main_test('x======5\necho $x', '> > =====5\n> ')
+    main_test('x=="323"=@asd\'asdasd\'\n echo $x', '> > =323=@asdasdasd\n> ')
 
 
 def test_spaces_in_names():
